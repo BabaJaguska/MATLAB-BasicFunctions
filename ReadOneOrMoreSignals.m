9a9817e2280415fb43c8c39f1ca22baf721d0602
Baba Jaguska <minja.n.belic@gmail.com>
2016-09-23 12:21:50 +0200

    Reads 1 or more .csv files
    Puts them in a cell array
    Returns the number of files as well

---
+++ ReadOneOrMoreSignals.m
@@ -0,0 +1,21 @@
+function [data, noFiles]=ReadOneOrMoreSignals()
+%%%% Reads one or more *.csv files
+%%%% Made primarily for EMG data recorded by bonestim/maxens hybrid stimulators
+%%%% but can work for other data too
+%%%% Returns data in a cell array (even if only 1 file)
+
+% READ FILES
+filenames=uigetfile('*.csv','Multiselect','ON');
+if ~ischar(filenames)
+    noFiles=length(filenames); 
+    data=cell(1,noFiles);
+    for i=1:noFiles
+        data{i}=csvread(filenames{i});
+    end
+else 
+    noFiles=1;
+    data{1}=csvread(filenames);
+end
+  
+end
+
